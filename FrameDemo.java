62e99c7d18f7980c39d7f44f0622b69a6d2769f6
nimson-s <nimson0628@gmail.com>
2026-03-16 00:15:47 -0700

    Add files via upload

---
+++ FrameDemo.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+ class  FrameDemo{
+    public static void main(String args[])
+ {
+      Frame f =new Frame();
+      f.setTitle("jayaraj bca");
+      f.setSize(500,500);
+      f.setVisible(true);
+ 
+    }
+}
